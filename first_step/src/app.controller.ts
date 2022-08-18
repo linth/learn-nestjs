78541bd01d15167b6d8fc2c7e280f2c7b73bdf6d
linth <zhua0404@gmail.com>
2022-08-18 17:25:25 +0800

    add example to practice.

--- first_step/src/app.controller.ts
+++ first_step/src/app.controller.ts
@@ -1,6 +1,9 @@
 import { Controller, Get } from '@nestjs/common';
+import { appendFile } from 'fs';
 import { AppService } from './app.service';
 
+
+// controller definition.
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
@@ -9,4 +12,11 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('/name')
+  getHelloName(): string {
+    return this.appService.getHelloName('george');
+  }
+
+  // method: get, post, patch, delete, ..., etc.
 }
--- first_step/src/app.service.ts
+++ first_step/src/app.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@nestjs/common';
 
+// service definition.
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  getHelloName(name: string): string {
+    return `Hello world! ${name}`;
   }
 }
