6f61bbbc291d63c54b80f18b3c73fdeb4ac674b0
linth <zhua0404@gmail.com>
2024-02-22 16:23:23 +0800

    feat: add postgres setting and module for database.

--- apps/typeorm-pg/src/typeorm-pg.module.ts
+++ apps/typeorm-pg/src/typeorm-pg.module.ts
@@ -2,9 +2,26 @@ import { Module } from '@nestjs/common';
 import { TypeormPgController } from './typeorm-pg.controller';
 import { TypeormPgService } from './typeorm-pg.service';
 import { PostModule } from './post/post.module';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from '@hapi/joi';
+import { DatabaseModule } from './database.module';
 
 @Module({
-  imports: [PostModule],
+  imports: [
+    // Validating environment variables
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        POSTGRES_HOST: Joi.string().required(),
+        POSTGRES_PORT: Joi.number().required(),
+        POSTGRES_USER: Joi.string().required(),
+        POSTGRES_PASSWORD: Joi.string().required(),
+        POSTGRES_DB: Joi.string().required(),
+        PORT: Joi.number(),
+      })
+    }),
+    DatabaseModule,
+    PostModule,
+  ],
   controllers: [TypeormPgController],
   providers: [TypeormPgService],
 })
